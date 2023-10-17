84f3498e11f45dd6c80969d0974d7dd660061a4e
ekoagungprasetyo <97371513+ekoagungprasetyo@users.noreply.github.com>
2023-10-17 21:25:15 +0700

    first commit

---
+++ sesi15.php
@@ -0,0 +1,55 @@
+<?php
+
+//no 1 
+$bilangan = 20;
+
+if ($bilangan % 2 == 0){
+    echo "Bilangan Genap";
+}
+else {
+    echo "Bilangan Ganjil";
+}
+
+//no 2
+
+$tahun = 2023
+
+if ($tahun % 4 == 0){
+    echo "Tahun Kabisat";
+}
+else {
+    echo "Bukan Tahun Kabisat"
+}
+
+//no 3 
+
+
+if ($nilai >= 85) {
+
+ $grade = 'A';
+
+ } elseif ($nilai >= 70) {
+
+ $grade = 'B';
+
+ } elseif ($nilai >= 55) {
+
+ $grade = 'C';
+
+ } elseif ($nilai >= 40) {
+
+ $grade = 'D';
+
+ } elseif ($nilai >0 ) {
+
+ $grade = 'E';
+
+ } else {
+
+        $grade = 'T';
+
+    }
+
+?>
+
+
